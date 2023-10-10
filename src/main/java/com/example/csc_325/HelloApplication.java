556792f0692a3a8951be3ad2ce361cce40cbc63f
marissalubow <marissalubow@gmail.com>
2023-10-10 19:02:59 -0400

    completed ToDos

--- src/main/java/com/example/csc_325/HelloApplication.java
+++ src/main/java/com/example/csc_325/HelloApplication.java
@@ -7,8 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
+public class HelloApplication {}
+
+//extends Application {
+    //@Override
+   /*
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
@@ -21,4 +24,7 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
+
+    */
